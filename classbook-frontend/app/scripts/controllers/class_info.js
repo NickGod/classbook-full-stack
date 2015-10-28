87f3a57e2f3bf69cc5ce63d70963083255319498
shuo-sun <shuosun.thomas@gmail.com>
2015-10-28 00:11:53 -0700

    calendar v2

--- classbook-frontend/app/scripts/controllers/class_info.js
+++ classbook-frontend/app/scripts/controllers/class_info.js
@@ -9,46 +9,57 @@
  */
 angular.module('classbookApp')
   .controller('ClassInfoCtrl', function($scope,$compile,uiCalendarConfig) {
-    var date = new Date();
-    var d = date.getDate();
-    var m = date.getMonth();
-    var y = date.getFullYear();
     
-    $scope.changeTo = 'Hungarian';
-    /* event source that pulls from google.com */
-    $scope.eventSource = {
-            color: 'yellow'
-    };
+    function getEventData() {
+      return [
+        {className: 'CS 130', startTime: '10:00:00', endTime: '11:50:00', days: [1, 3]},
+        {className: 'CS 145', startTime: '10:00:00', endTime: '11:50:00', days: [2, 4]},
+        {className: 'ECON 106V', startTime: '13:00:00', endTime: '14:15:00', days: [2, 4]},
+        {className: 'HIST 1C', startTime: '14:00:00', endTime: '15:00:00', days: [1, 3, 5]},
+        {className: 'MGMT 120A', startTime: '08:00:00', endTime: '09:00:00', days: [1, 3, 5]},
+        // {className: 'MATH 131A', startTime: '09:00:00', endTime: '10:00:00', days: [2, 4]},
+      ];
+    }
+    
+    var quarterBegins = new Date('2015-09-22');
+    var quarterEnds = new Date('2015-12-12');
+    
+    function getEvents() {
+      var data = getEventData();
+      var events = [];
+      var date;
+      
+      for(date = new Date(quarterBegins.getTime()); date < quarterEnds; date.setDate(date.getDate() + 1)) {
+        var course, i;
+        for (i = 0; i < data.length; i++) {
+          course = data[i];
+          if (course.days.indexOf(date.getDay()) != -1) {
+            var timeBegin = date.toDateString() + ' ' + course.startTime;
+            var timeEnd = date.toDateString() + ' ' + course.endTime;
+            
+            events.push({
+              title: course.className,
+              start: new Date(timeBegin),
+              end: new Date(timeEnd),
+              allDay: false,
+              editable: false
+            });
+          }
+        }
+      }
+      
+      return events;
+    }
+    
+    /* event source that pulls from url */
+    $scope.eventSource = {};
+    
     /* event source that contains custom events on the scope */
-    $scope.events = [
-      {title: 'All Day Event',start: new Date(y, m, 1, 15), end: new Date(y, m, 1, 16), editable: false},
-      {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
-      {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
-      {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
-      {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
-      {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
-    ];
-    /* event source that calls a function on every view switch */
-    $scope.eventsF = function (start, end, timezone, callback) {
-      var s = new Date(start).getTime() / 1000;
-      var e = new Date(end).getTime() / 1000;
-      var m = new Date(start).getMonth();
-      var events = [{title: 'Feed Me ' + m,start: s + (50000),end: s + (100000),allDay: false, className: ['customFeed']}];
-      callback(events);
-    };
+    $scope.events = getEvents();
 
-    $scope.calEventsExt = {
-       color: '#f00',
-       textColor: 'yellow',
-       events: [ 
-          {type:'party',title: 'Lunch',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
-          {type:'party',title: 'Lunch 2',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
-          {type:'party',title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
-        ]
-    };
     /* alert on eventClick */
-    $scope.alertOnEventClick = function( date, jsEvent, view){
-        $scope.alertMessage = (date.title + ' was clicked ');
+    $scope.alertOnEventClick = function(course, jsEvent, view){
+        $scope.alertMessage = (course.title + ': info... ');
     };
     /* alert on Drop */
      $scope.alertOnDrop = function(event, delta, revertFunc, jsEvent, ui, view){
@@ -117,18 +128,10 @@ angular.module('classbookApp')
       }
     };
 
-    $scope.changeLang = function() {
-      if($scope.changeTo === 'Hungarian'){
-        $scope.uiConfig.calendar.dayNames = ["Vasárnap", "Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat"];
-        $scope.uiConfig.calendar.dayNamesShort = ["Vas", "Hét", "Kedd", "Sze", "Csüt", "Pén", "Szo"];
-        $scope.changeTo= 'English';
-      } else {
-        $scope.uiConfig.calendar.dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        $scope.uiConfig.calendar.dayNamesShort = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-        $scope.changeTo = 'Hungarian';
-      }
-    };
+    $scope.uiConfig.calendar.dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    $scope.uiConfig.calendar.dayNamesShort = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
     /* event sources array*/
-    $scope.eventSources = [$scope.events, $scope.eventSource, $scope.eventsF];
-    $scope.eventSources2 = [$scope.calEventsExt, $scope.eventsF, $scope.events];
+    $scope.eventSources = [$scope.events, $scope.eventSource];
 });
+

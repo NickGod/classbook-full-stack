11fee94546b5a2af3734bf4845262e979106f258
Zhengliang Wu <lanzhou522@gmail.com>
2015-10-28 19:49:06 -0700

    change days representation in seed

--- classbook/db/seeds.rb
+++ classbook/db/seeds.rb
@@ -17,29 +17,29 @@
 
 
 Lecture.create([
-	{ name: 'CS130', begTime: '12:00P', endTime: '1:50P', days:'MW', location: 'BOELTER'},
-	{ name: 'CS31', begTime: '4:00P', endTime: '5:50P', days:'MW', location: 'BOELTER'},
-	{ name: 'CS32', begTime: '8:00A', endTime: '9:50A', days:'MW', location: 'BOELTER'},
-	{ name: 'CS35L', begTime: '12:00P', endTime: '1:50P', days:'TR', location: 'BOELTER'},
-	{ name: 'CS111', begTime: '2:00P', endTime: '3:50P', days:'TR', location: 'BOELTER'},
-	{ name: 'CS112', begTime: '12:00P', endTime: '1:50P', days:'MW', location: 'BOELTER'},
-	{ name: 'CS118', begTime: '12:00P', endTime: '1:50P', days:'MW', location: 'BOELTER'},
-	{ name: 'CS1131', begTime: '12:00P', endTime: '1:50P', days:'TR', location: 'BOELTER'},
-	{ name: 'CS143', begTime: '8:00A', endTime: '9:50P', days:'MW', location: 'BOELTER'},
-	{ name: 'CS144', begTime: '10:00P', endTime: '11:50P', days:'MW', location: 'BOELTER'},
-	{ name: 'CS161', begTime: '12:00P', endTime: '1:50P', days:'TR', location: 'BOELTER'},
-	{ name: 'CS170A', begTime: '2:00P', endTime: '3:50P', days:'MW', location: 'BOELTER'},
-	{ name: 'CS174A', begTime: '4:00P', endTime: '5:50P', days:'MW', location: 'BOELTER'},
-	{ name: 'CS180', begTime: '6:00P', endTime: '7:50P', days:'TR', location: 'BOELTER'},
-	{ name: 'CS181', begTime: '12:00P', endTime: '1:50P', days:'MW', location: 'BOELTER'}
+	{ name: 'CS130', begTime: '12:00P', endTime: '1:50P', days:'13', location: 'BOELTER'},
+	{ name: 'CS31', begTime: '4:00P', endTime: '5:50P', days:'13', location: 'BOELTER'},
+	{ name: 'CS32', begTime: '8:00A', endTime: '9:50A', days:'13', location: 'BOELTER'},
+	{ name: 'CS35L', begTime: '12:00P', endTime: '1:50P', days:'24', location: 'BOELTER'},
+	{ name: 'CS111', begTime: '2:00P', endTime: '3:50P', days:'24', location: 'BOELTER'},
+	{ name: 'CS112', begTime: '12:00P', endTime: '1:50P', days:'13', location: 'BOELTER'},
+	{ name: 'CS118', begTime: '12:00P', endTime: '1:50P', days:'13', location: 'BOELTER'},
+	{ name: 'CS1131', begTime: '12:00P', endTime: '1:50P', days:'24', location: 'BOELTER'},
+	{ name: 'CS143', begTime: '8:00A', endTime: '9:50P', days:'13', location: 'BOELTER'},
+	{ name: 'CS144', begTime: '10:00P', endTime: '11:50P', days:'13', location: 'BOELTER'},
+	{ name: 'CS161', begTime: '12:00P', endTime: '1:50P', days:'24', location: 'BOELTER'},
+	{ name: 'CS170A', begTime: '2:00P', endTime: '3:50P', days:'13', location: 'BOELTER'},
+	{ name: 'CS174A', begTime: '4:00P', endTime: '5:50P', days:'13', location: 'BOELTER'},
+	{ name: 'CS180', begTime: '6:00P', endTime: '7:50P', days:'24', location: 'BOELTER'},
+	{ name: 'CS181', begTime: '12:00P', endTime: '1:50P', days:'13', location: 'BOELTER'}
 ])
 
 Discussion.create([
-	{ lectureId: 1, begTime: '10:00A', endTime: '11:50P', days:'F', location: 'BOELTER'},
-	{ lectureId: 1, begTime: '2:00P', endTime: '3:50P', days:'F', location: 'BOELTER'},
-	{ lectureId: 1, begTime: '4:00P', endTime: '5:50P', days:'F', location: 'BOELTER'},
-	{ lectureId: 2, begTime: '12:00A', endTime: '11:50P', days:'F', location: 'BOELTER'},
-	{ lectureId: 2, begTime: '2:00P', endTime: '3:50P', days:'F', location: 'BOELTER'}
+	{ lectureId: 1, begTime: '10:00A', endTime: '11:50P', days:'5', location: 'BOELTER'},
+	{ lectureId: 1, begTime: '2:00P', endTime: '3:50P', days:'5', location: 'BOELTER'},
+	{ lectureId: 1, begTime: '4:00P', endTime: '5:50P', days:'5', location: 'BOELTER'},
+	{ lectureId: 2, begTime: '12:00A', endTime: '11:50P', days:'5', location: 'BOELTER'},
+	{ lectureId: 2, begTime: '2:00P', endTime: '3:50P', days:'5', location: 'BOELTER'}
 
 ])
 

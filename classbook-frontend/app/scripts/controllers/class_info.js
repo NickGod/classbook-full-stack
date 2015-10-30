3028622ea655eb47119851181ee217e07128a149
shuo-sun <shuosun.thomas@gmail.com>
2015-10-30 00:23:43 -0700

    fix bugs and improve add class

--- classbook-frontend/app/scripts/controllers/class_info.js
+++ classbook-frontend/app/scripts/controllers/class_info.js
@@ -44,37 +44,48 @@ angular.module('classbookApp')
         {className: 'MGMT 120A', startTime: '08:00:00', endTime: '09:00:00', days: [1, 3, 5]},
       ];
     }
-
+    
     var quarterBegins = new Date('2015-09-22');
     var quarterEnds = new Date('2015-12-12');
+    
+    function parseData(data) {
+      var events = [];
+      var date;
+
+      for(date = new Date(quarterBegins.getTime()); date < quarterEnds; date.setDate(date.getDate() + 1)) {
+        var course, i;
+        for (i = 0; i < data.length; i++) {
+          course = data[i];
+          if (course.days.indexOf(date.getDay()) != -1) {
+            var timeBegin = date.toDateString() + ' ' + course.startTime;
+            var timeEnd = date.toDateString() + ' ' + course.endTime;
+
+            events.push({
+              title: course.className,
+              start: new Date(timeBegin),
+              end: new Date(timeEnd),
+              allDay: false,
+              editable: false,
+              stick: true,
+            });
+          }
+        }
+      }
+      
+      return events;
+    }
+    
+    /* event source that pulls from url */
+    $scope.eventSource = {};
+
+    /* event source that contains custom events on the scope */
+    $scope.events = [];
 
     function getEvents() {
       var user = AuthService.currentUser();
       
       user.getAllEnrolledClasses().then(function(resp) {
-        var data = resp.data;
-        var events = [];
-        var date;
-  
-        for(date = new Date(quarterBegins.getTime()); date < quarterEnds; date.setDate(date.getDate() + 1)) {
-          var course, i;
-          for (i = 0; i < data.length; i++) {
-            course = data[i];
-            if (course.days.indexOf(date.getDay()) != -1) {
-              var timeBegin = date.toDateString() + ' ' + course.startTime;
-              var timeEnd = date.toDateString() + ' ' + course.endTime;
-  
-              events.push({
-                title: course.className,
-                start: new Date(timeBegin),
-                end: new Date(timeEnd),
-                allDay: false,
-                editable: false
-              });
-            }
-          }
-        }
-        
+        var events = parseData(resp.data);
         $scope.events.push.apply($scope.events, events);
       })
       .catch(function(resp) {
@@ -82,12 +93,6 @@ angular.module('classbookApp')
       });
       
     }
-
-    /* event source that pulls from url */
-    $scope.eventSource = {};
-
-    /* event source that contains custom events on the scope */
-    $scope.events = [];
 
     /* alert on eventClick */
     $scope.alertOnEventClick = function(course, jsEvent, view){
@@ -142,17 +147,72 @@ angular.module('classbookApp')
     };
 
     $scope.addClass = function(course) {
-      console.log(course.name);
+      alert('Class ' + course.name + ' and discussion ' + course.dis.name + ' have been added');
+      var newEvents = [];
+      var days = [];
+      if (course.mon) {
+        days.push(1);
+      }
+      if (course.tue) {
+        days.push(2);
+      }
+      if (course.wed) {
+        days.push(3);
+      }
+      if (course.thur) {
+        days.push(4);
+      }
+      if (course.fri) {
+        days.push(5);
+      }
+      
+      newEvents.push({
+        className: course.name,
+        startTime: course.start,
+        endTime: course.end,
+        days: days,
+      });
+      
+      var disDays = [];
+      if (course.dis.mon) {
+        disDays.push(1);
+      }
+      if (course.dis.tue) {
+        disDays.push(2);
+      }
+      if (course.dis.wed) {
+        disDays.push(3);
+      }
+      if (course.dis.thur) {
+        disDays.push(4);
+      }
+      if (course.dis.fri) {
+        disDays.push(5);
+      }
+      
+      newEvents.push({
+        className: course.dis.name,
+        startTime: course.dis.start,
+        endTime: course.dis.end,
+        days: disDays,
+      });
+      
+      var events = parseData(newEvents);
+      $scope.events.push.apply($scope.events, events);
     };
 
     $scope.test = function() {
       alert('test function called');
     };
+    
+    $scope.viewRender = function(view, element) {
+      // $('#calendar').fullCalendar('updateEvent', $scope.events);
+    };
 
     /* config object */
     $scope.uiConfig = {
       calendar:{
-        height: 450,
+        height: 600,
         editable: true,
         header: {
           left: 'title',
@@ -167,12 +227,12 @@ angular.module('classbookApp')
       }
     };
 
-
     $scope.uiConfig.calendar.dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     $scope.uiConfig.calendar.dayNamesShort = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
     /* event sources array*/
     $scope.eventSources = [$scope.events, $scope.eventSource];
+
     getEvents();
   }
 ]);

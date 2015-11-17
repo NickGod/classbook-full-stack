db62eaad6a5642f78e83634429f2ca806d398b80
Tianyi <tianyixia55@gmail.com>
2015-11-17 15:44:46 -0800

    merged

--- classbook/db/schema.rb
+++ classbook/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151116051131) do
+ActiveRecord::Schema.define(version: 20151116080844) do
 
   create_table "discussions", force: :cascade do |t|
     t.integer  "lectureId",  limit: 4

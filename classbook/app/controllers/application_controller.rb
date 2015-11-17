3f8389db605d36013a08402138a653b83ffbbf1c
Tianyi <tianyixia55@gmail.com>
2015-11-17 15:31:50 -0800

    Improve error handling

--- classbook/app/controllers/application_controller.rb
+++ classbook/app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::API
   	discussion = params['discussion']
 
   	if name.nil? && department.nil? || name.nil? && !discussion.nil?
-  		render json: { :error => '400 Bad Request'} 
+  		render json: {"error":true,"errormsg":"invalid parameters"} , status: :bad_request
   		return
   	end
 

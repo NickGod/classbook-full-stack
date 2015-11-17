6340d32309a858017fcf5de474f1ad6ca10c8ff9
mengyuan28 <mengyuan.yu28@gmail.com>
2015-11-16 20:56:49 -0800

    user_info page

--- classbook-frontend/app/scripts/controllers/user_info.js
+++ classbook-frontend/app/scripts/controllers/user_info.js
@@ -8,10 +8,17 @@
  * Controller of the classbookApp
  */
 angular.module('classbookApp')
-  .controller('UserInfoCtrl', function () {
+  .controller('UserInfoCtrl', [
+  '$scope','AuthService',
+  function ($scope, AuthService) {
+   $scope.auth = AuthService;
+   $scope.tab = 1;
+   $scope.user_name = "Mengyuan";
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+
+}
+]);
